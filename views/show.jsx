d8f227edfefa881fd12e8526e0477ccec12603ed
CodyCork <codycork.sfg@outlook.com>
2022-08-03 21:39:15 -0500

    Edit Button Works

--- views/show.jsx
+++ views/show.jsx
@@ -1,30 +1,31 @@
-const React = require('react')
-const Default = require('./layouts/default')
+const React = require("react");
+const Default = require("./layouts/Default");
 
-function Show ({bread,index}) {
+function Show({ bread, index }) {
   // confirm we are getting our bread data in terminal
-  // console.log(bread.name)
-    return (
-      <Default>
-        <h3>{bread.name}</h3>
-        <p>
-          and it
-          {
-            bread.hasGluten
-            ? <span> does </span>
-            : <span> does NOT </span>
-          }
-          have gluten.
-        </p>
-        <img src={bread.image} alt={bread.name} />
-        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-        <form action={`/breads/${index}?_method=DELETE`} method="POST">
-          <input type='submit' value="DELETE"/>
-        </form>
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <p>Baked by {bread.baker}</p>
+      <a href={`/breads/${bread.id}/edit`}>
+        <button>Edit</button>
+      </a>
 
-        <li><a href="/breads">Go home</a></li>
-      </Default>
-    )
+      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+        <input type="submit" value="DELETE" />
+      </form>
+
+      <li>
+        <a href="/breads">Go home</a>
+      </li>
+    </Default>
+  );
 }
 
-module.exports = Show
+module.exports = Show;
